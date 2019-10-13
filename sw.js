0a0d7f637b3d571f31916708aa324d3a299a5b44
pupubird <rainchai4240@gmail.com>
2019-10-14 00:10:37 +0800

    Transformed to PWA

---
+++ sw.js
@@ -0,0 +1,37 @@
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('putodo').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/index.html?homescreen=1',
+                '/?homescreen=1',
+                '/assets/check.svg',
+                '/assets/check.png',
+                '/css/style.css',
+                '/js/app.js'
+            ]);
+        })
+    );
+});
+
+
+
+self.addEventListener('fetch', function (event) {
+
+    console.log(event.request.url);
+
+    event.respondWith(
+
+        caches.match(event.request).then(function (response) {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
