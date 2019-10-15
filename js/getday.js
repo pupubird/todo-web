6288187c339637c1e7ceaa367a44bcb43676f5af
pupubird <rainchai4240@gmail.com>
2019-10-15 23:22:18 +0800

    debug

---
+++ js/getday.js
@@ -0,0 +1,38 @@
+function display_date() {
+    let day_date = document.getElementById('day_date');
+    let getD = new Date();
+    let today = getDayToString(getD.getDay());
+    day_date.innerText = today + ', '
+        + getD.getDate() + '/'
+        + getD.getMonth() + '/'
+        + getD.getFullYear();
+}
+
+function getDayToString(today) {
+    let day = '';
+    switch (today) {
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        case 0:
+            day = "Sunday";
+            break;
+    }
+    return 'Tuesday';
+}
+display_date();
