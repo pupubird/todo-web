dea9a669adfe9c95dceed29291a05617b93efaef
pupubird <rainchai4240@gmail.com>
2019-10-15 23:20:12 +0800

    debug

--- js/app.js
+++ js/app.js
@@ -124,41 +124,5 @@ function display_taskCount() {
     var task_count = document.getElementById('active_tasks');
     task_count.innerText = getTasks() + ' Active Tasks';
 }
-function display_date() {
-    let day_date = document.getElementById('day_date');
-    let getD = new Date();
-    let today = getDayToString(getD.getDay());
-    day_date.innerText = today + ', '
-        + getD.getDate() + '/'
-        + getD.getMonth() + '/'
-        + getD.getFullYear();
-}
 
-function getDayToString(today) {
-    let day = '';
-    switch (today) {
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        case 0:
-            day = "Sunday";
-    }
-    return day;
-}
-display_date();
 display_taskCount();
